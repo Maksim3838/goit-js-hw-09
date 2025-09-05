cf5b9cb8ac12d27a9b891a3171f35532ac8a01a5
Maksim3838 <70702008m@gmail.com>
2025-09-05 17:42:03 +0000

    Deploying to gh-pages from @ Maksim3838/goit-js-hw-09@492fe3ec1d8cba8c05a9991dc4777e5a74f8030d 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const t={email:"",message:""},m=document.querySelector(".feedback-form");m.addEventListener("input",a=>{const e=a.target;(e.name==="email"||e.name==="message")&&(t[e.name]=e.value),localStorage.setItem("feedback-form-state",JSON.stringify(t))});
+//# sourceMappingURL=2-form.js.map

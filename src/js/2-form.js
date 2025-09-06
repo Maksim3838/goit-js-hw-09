9911473f937d299db32fac4aee8ae5f0ce54c2c9
Maksim <70702008m@gmail.com>
2025-09-06 10:53:27 +0300

    texty

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,13 +1,36 @@
-const formData = {
+let formData = {
   email: "",
   message: ""
 };
 
 const form = document.querySelector(".feedback-form");
+
+const savedData = localStorage.getItem("feedback-form-state");
+
+if (savedData) {
+  formData = JSON.parse(savedData);
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
 form.addEventListener("input", (event) => {
-  const target = event.target; 
-    if (target.name === "email" || target.name === "message") {
-    formData[target.name] = target.value; 
-  }
+  if (event.target.name in formData) {
+    formData[event.target.name] = event.target.value;
     localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (formData.email.trim() === "" || formData.message.trim() === "") {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log("Submitted data:", formData);
+
+    form.reset();
+  localStorage.removeItem("feedback-form-state");
+  formData = { email: "", message: "" };
 });
